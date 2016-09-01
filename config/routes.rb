b77d7d2f1b4b009f7a1acb026544483391f04727
Alan <alanoidster@gmail.com>
2016-09-01 12:42:39 +0800

    OAuth FB fixes, generate Cart views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  get 'cart/show'
+
+  get 'cart/create'
+
+  get 'cart/update'
+
+  get 'cart/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'landing#index'
   get :about, to: 'about#aboutpage'
   get :items, to: 'items#index'
   get :faq, to: 'faq#index'
 
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', omniauth_callbacks: 'users/omniauth_callbacks' }
+  # devise_scope :user, only: [:destroy] do
+  #   delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  match 'auth/:provider/callback', to: 'omniauth_callbacks#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
