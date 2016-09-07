9ed39614e9833ca4ae77eb5f1eada18105b04dfe
Alan <alanoidster@gmail.com>
2016-09-07 19:03:09 +0800

    Modal cart fixes

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,36 +2,7 @@ class CartController < ApplicationController
   respond_to :js
   before_action :authenticate_user!
   before_action :load_cart
-  after_action :write_cart, only: [:add_item, :remove_item, :update_item]
-
-  def show
-    @items = []
-    @total_price = 0.0;
-
-    @cart.each do |k,v|
-      item = Item.find_by(id: k)
-
-      if !Item.find_by(id: k)
-        flash[:danger] = "Your item is not valid!"
-        break
-      end
-
-        total_item_price = 0
-        total_item_price = item.price * v.to_f
-        @total_price += total_item_price
-
-        item.define_singleton_method(:quantity) {v}
-        item.define_singleton_method(:total) {total_item_price}
-        @items << item
-
-    end
-
-
-    rescue => e
-      redirect_to items_path
-      flash[:danger] = "Your cart is empty!"
-  end
-
+  after_action :write_cart, only: [:remove_item, :update_item]
 
   def add_item
     if @cart[params[:id]]
@@ -42,7 +13,7 @@ class CartController < ApplicationController
     else
       @cart[params[:id]] = params[:quantity]
     end
-      # cookies[:cart] = JSON.generate(@cart)
+      cookies[:cart] = JSON.generate(@cart)
   end
 
 
@@ -51,13 +22,11 @@ class CartController < ApplicationController
     if @cart[params[:id]]
       @cart[params[:id]] = params[:quantity]
     end
-      redirect_to cart_show_path
       flash[:success] = "You've updated your cart."
   end
 
   def remove_item
       @cart.delete params[:id]
-      redirect_to cart_show_path
       flash[:danger] = "Item removed!"
   end
 
@@ -72,7 +41,6 @@ class CartController < ApplicationController
   def write_cart
     cookies[:cart] = JSON.generate(@cart)
   end
-
 
 
 end
--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,2 +1,21 @@
 module CartHelper
+
+  def carts_hash
+    return {} unless cookies[:cart]
+
+    cart =  JSON.parse(cookies[:cart])
+    total_price = 0
+    items = []
+
+    cart.each do |item_id, quantity|
+      item = Item.find_by(id: item_id)
+
+      item.define_singleton_method(:quantity) { quantity }
+      item.define_singleton_method(:total) { price * quantity.to_f }
+      total_price += item.price * quantity.to_f
+      items << item
+    end
+
+    { total_price: total_price, items: items }
+  end
 end
