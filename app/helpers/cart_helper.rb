115324c35131c6a0ce74112d71c2d6e0ebadbdfd
leemengchen <orange_2039@hotmail.com>
2016-09-08 12:06:18 +0800

    carousel, sliding image done

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -10,10 +10,15 @@ module CartHelper
     cart.each do |item_id, quantity|
       item = Item.find_by(id: item_id)
 
+      if !item
+        next
+      end
+
       item.define_singleton_method(:quantity) { quantity }
       item.define_singleton_method(:total) { price * quantity.to_f }
       total_price += item.price * quantity.to_f
       items << item
+
     end
 
     { total_price: total_price, items: items }
